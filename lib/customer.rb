e32f3d9fec19d6e173d438c298ed3dc18e398625
Eugene White <eugenewhite1994@gmail.com>
2020-08-12 23:29:28 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,16 @@
-class Customer
-  
-end
+class Appointment
+
+  attr_accessor :date, :patient, :doctor
+  @@all = []
+
+  def initialize(patient, date, doctor)
+    @patient = patient
+    @date = date
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end 

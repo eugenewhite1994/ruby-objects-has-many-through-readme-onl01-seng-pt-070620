24eed07a1bc0fa952203fc5f6de7164d898635aa
Eugene White <eugenewhite1994@gmail.com>
2020-08-12 23:42:35 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,9 +1,10 @@
 class Meal
- 
+
+
   attr_accessor :waiter, :customer, :total, :tip
- 
+
   @@all = []
- 
+
   def initialize(waiter, customer, total, tip=0)
     @waiter = waiter
     @customer = customer
@@ -11,7 +12,7 @@ class Meal
     @tip = tip
     @@all << self
   end
- 
+
   def self.all
     @@all
   end

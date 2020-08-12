14ddbe8f05b64bc1f717235832f0eb268a7b35a2
Eugene White <eugenewhite1994@gmail.com>
2020-08-12 23:33:04 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,18 +1,34 @@
-class Appointment
+class Customer
+    attr_accessor :name, :age
 
-  attr_accessor :date, :patient, :doctor
+    @@all = []
 
-  @@all = []
+    def initialize(name, age)
+        @name = name
+        @age = ArgumentError
 
-  def initialize(date, patient, doctor)
-    @date = date
-    @patient = patient
-    @doctor = doctor
-    @@all << self
-  end
+        @@all << self
+    end
 
-  def self.all
-    @@all
-  end
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+        meals.map do |meal|
+            meal.waiter
+        end
+    end 
+
+    def self.all
+        @@all
+    end
+
 
 end
